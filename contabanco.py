603747da71a6e720d2fcffc2843e87882bc7c922
João Neto <joaoneto.20fsa@gmail.com>
2020-08-27 16:23:19 -0300

    Update contabanco.py

--- contabanco.py
+++ contabanco.py
@@ -9,21 +9,27 @@ class ContaBanco():
         self.limite_saque = limite_saque
     
     def sacar(self, valor):
-        self.saldo -= valor
+        if valor > 0 and valor <= self.saldo and valor <= self.limite_saque:
+            self.saldo -= valor
+        else:
+            print("Não foi possível sacar o valor")
 
     def depositar(self, valor):
-        self.saldo += valor
+        if valor > 0:
+            self.saldo += valor
+        else:
+            print("Não foi possível depositar o valor")
 
     def imprimir_dados(self):
-        print('''
+        return ('''
             Informações sobre a conta:
             Nome do banco: {}
             Número da agência: {}
             Número da conta: {}
             Nome do titular: {}
             Tipo: {}
-            Saldo: {}
-            Limite de saque: {}
+            Saldo: R${}
+            Limite de saque: R${}
         '''.format(
             self.nome_banco,
             self.agencia,
@@ -38,11 +44,12 @@ def main():
     conta1 = ContaBanco("Nubank", 0000, 1111, "Godofredo", "P", 2000, 1000)
 
     print("Saldo inicial da conta: R${}".format(conta1.saldo))
-    conta1.sacar(100)
-    print("Após o saque de R$100, Saldo na conta: R${}".format(conta1.saldo))
+    conta1.sacar(1000)
+    print("Após o saque de R$1000, Saldo na conta: R${}".format(conta1.saldo))
 
-    conta1.depositar(200)
-    print("Saldo da conta, após o deposito de R$200: {}".format(conta1.saldo))
+    conta1.depositar(10)
+    print("Saldo da conta após o deposito de R$10: R${}".format(conta1.saldo))
 
-    conta1.imprimir_dados()
+    infos_conta = conta1.imprimir_dados()
+    print(infos_conta)
 main()

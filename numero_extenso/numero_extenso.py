7c153cce116594f17c879b97f8d64aaa64440f14
João Neto <joaoneto.20fsa@gmail.com>
2020-09-08 16:01:02 -0300

    Update numero_extenso.py

--- numero_extenso/numero_extenso.py
+++ numero_extenso/numero_extenso.py
@@ -1,40 +1,59 @@
-def splitNum(num):
-    digitList = []
+def assembleDozens(num, number_in_full, units, dozens, others_dozens): 
+    # numbers less than 19
+    if num < 20:
+        for key, value in dozens.items():
+            if num == key:
+                number_in_full += value
+    # numbers greater than 19
+    else:
+        # dozens
+        dozens = (num // 10) * 10
+        for key, value in others_dozens.items():
+            if dozens == key:
+                number_in_full += value
+        # unit
+        unit = num % 10
+        for key, value in units.items():
+            if unit != 0 and unit == key:
+                number_in_full += " e {}".format(value)
+
+    return number_in_full
+
+def assembleNumbers(num, number_in_full, units, dozens, others_dozens):
     lenNum = len(str(num))
 
     if lenNum == 1:
-        digitList.append(num)
+        # positives
+        for key, value in units.items():
+            if num == key:
+                number_in_full += value
     elif lenNum == 2:
+        # positives
         if num > 0:
-            digitList.append(num // 10)
-            digitList.append(num % 10)
+            number_in_full += assembleDozens(num, number_in_full, units, dozens, others_dozens)
+        # negatives
         else:
-            digitList.append("-")
-            digitList.append(abs(num))
+            unit = abs(num)
+            for key, value in units.items():
+                if unit == key:
+                    number_in_full += "menos {}".format(value)
     elif lenNum == 3:
-        digitList.append("-")
-        digitList.append(abs(num) // 10)
-        digitList.append(abs(num) % 10)
+        # negatives
+        number_in_full += "menos " + assembleDozens(abs(num), number_in_full, units, dozens, others_dozens)
 
-    return digitList
+    return number_in_full
 
-def writeNum(num):
-    unit = {0: "zero", 1: "um", 2: "dois", 3: "três", 4: "quatro", 5: "cinco", 6: "seis", 7: "sete", 8: "oito", 9: "nove"}
+def main():
+    number_in_full = ""
 
-    dozens = {11: "onze", 12: "doze", 13: "treze", 14: "quatorze", 15: "quinze", 16: "dezesseis", 17: "dezessete", 18: "dezoito", 19: "dezenove"}
+    units = {0: "zero", 1: "um", 2: "dois", 3: "três", 4: "quatro", 5: "cinco", 6: "seis", 7: "sete", 8: "oito", 9: "nove"}
+
+    dozens = {10: "dez", 11: "onze", 12: "doze", 13: "treze", 14: "quatorze", 15: "quinze", 16: "dezesseis", 17: "dezessete", 18: "dezoito", 19: "dezenove"}
 
     others_dozens = {20: "vinte", 30: "trinta", 40: "quarenta", 50: "cinquenta", 60: "sessenta", 70: "setenta", 80: "oitenta", 90: "noventa"}
 
-    for k, v in others_dozens.items():
-        print(k, v)
-
-def main():
     num = int(input("Digite um número inteiro entre -99 e 99 \n"))
+    written_number = assembleNumbers(num, number_in_full, units, dozens, others_dozens)
+    print(written_number)
     
-    digitList = splitNum(num)
-    print(digitList)
-
-    writeNum(num)
-    
-if __name__ == "__main__":
-    main()
+main()

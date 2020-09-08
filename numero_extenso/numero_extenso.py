6a757344453a29b6eabec9c647bc16c55f361090
João Neto <joaoneto.20fsa@gmail.com>
2020-09-08 14:51:54 -0300

    Update numero_extenso.py

--- numero_extenso/numero_extenso.py
+++ numero_extenso/numero_extenso.py
@@ -18,11 +18,23 @@ def splitNum(num):
 
     return digitList
 
+def writeNum(num):
+    unit = {0: "zero", 1: "um", 2: "dois", 3: "três", 4: "quatro", 5: "cinco", 6: "seis", 7: "sete", 8: "oito", 9: "nove"}
+
+    dozens = {11: "onze", 12: "doze", 13: "treze", 14: "quatorze", 15: "quinze", 16: "dezesseis", 17: "dezessete", 18: "dezoito", 19: "dezenove"}
+
+    others_dozens = {20: "vinte", 30: "trinta", 40: "quarenta", 50: "cinquenta", 60: "sessenta", 70: "setenta", 80: "oitenta", 90: "noventa"}
+
+    for k, v in others_dozens.items():
+        print(k, v)
+
 def main():
     num = int(input("Digite um número inteiro entre -99 e 99 \n"))
     
     digitList = splitNum(num)
     print(digitList)
+
+    writeNum(num)
     
 if __name__ == "__main__":
     main()
